3c71cf631b030352f88ed38f9e4e9e31f3d3745f
Rohit Wason <wasonrohit@gmail.com>
2020-05-24 11:01:42 -0400

    added server for heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var serveStatic = require("serve-static");
+var app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("server started " + port);
